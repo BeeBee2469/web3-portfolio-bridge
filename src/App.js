dc8e4f342a540c1e81f276af484d7f187394fd4f
BeeBee2469 <brightbenson977@gmail.com>
2026-06-20 09:56:24 +0100

    Fix import case and center h1 styling
    Updated import statement and modified h1 style.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Web3Bridge from './web3Bridge';
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', fontFamily: 'sans-serif' }}>My Web3 App</h1>
+      <Web3Bridge />
+    </div>
+  );
+}
